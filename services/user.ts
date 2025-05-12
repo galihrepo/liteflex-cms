eae14049196e1cbf0aecd860c529533fad4699e3
Galih <galih@ruangguru.com>
2025-05-12 22:34:04 +0700

    add try catch

--- services/user.ts
+++ services/user.ts
@@ -6,17 +6,22 @@ import { db } from "../config/configFirebase";
 export const saveUser = async (user: User) => {
   if (!user) return;
 
-  const userRef = doc(db, "employee", user.uid || '');
-  const existingDoc = await getDoc(userRef);
+  try {
+    const userRef = doc(db, "employee", user.uid || '');
+    const existingDoc = await getDoc(userRef);
 
-  if (!existingDoc.exists()) {
-    const userData = {
-        dealersId: `/dealers/${Constants.expoConfig?.extra?.dealer}`,
-        email: user.email || "",
-        name: user.displayName || "",
-        phone: user.phoneNumber || "",
-    };
+    if (!existingDoc.exists()) {
+        const userData = {
+            dealersId: `/dealers/${Constants.expoConfig?.extra?.dealer}`,
+            email: user.email || "",
+            name: user.displayName || "",
+            phone: user.phoneNumber || "",
+        };
 
-    await setDoc(userRef, userData);    
+        await setDoc(userRef, userData);    
+    }
+  } catch (e) {
+    console.error(e);
   }
+  
 };
